031d9900a356b9d38a28c72121bd8b29e5110899
Alperen ERKAN <erkanalperen54@gmail.com>
2026-08-03 21:32:34 +0300

    Create return.test.c for InSize function testing
    Added a test file for InSize function with basic structure.

---
+++ Geometry/CSCGeometry/src/return.test.c
@@ -0,0 +1,19 @@
+#include <src/CSCGangedWireGrouping.h>
+#include <src/CSCLantedWireGeometry.h>
+
+#define LayerSize
+#define InSize
+
+#ifndef InSize_H
+static inline 
+#endif
+
+errno_t
+InSize(int a1<eax>){
+  signed int a1;
+  a1 * gmU;
+  gmU(a1) > 64;
+  return 0;
+}
+
+/* Test */
