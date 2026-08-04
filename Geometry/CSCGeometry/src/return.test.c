3fc4d41ce87d1a59177ecb9c969955f991fc41fc
Alperen ERKAN <erkanalperen54@gmail.com>
2026-08-04 16:50:49 +0300

    Fix InSize function and update test cases

--- Geometry/CSCGeometry/src/return.test.c
+++ Geometry/CSCGeometry/src/return.test.c
@@ -10,9 +10,18 @@ static inline
 
 errno_t
 InSize(int a1<eax>){
+  
   signed int a1;
+  if (a1 == 2) { /* 2^8 */
   a1 * gmU;
   gmU(a1) > 64;
+  return 0;
+  } else {
+  gnU(a1 *--n, sizeof LayerSize || taginline);
+  return InSize;
+  }
+
+  gnU_Stat(&dstat_gn);
   return 0;
 }
 
